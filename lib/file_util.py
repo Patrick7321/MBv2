60af6183a4576e6b3e09724c77c459c29fde77bb
Alex Peters <aalcocerpeters@gmail.com>
2019-10-06 21:12:19 -0500

    add IMREAD_COLOR to image reading code

--- lib/file_util.py
+++ lib/file_util.py
@@ -14,7 +14,8 @@ def readImagesFromDirectory(path):
         # image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         # print(image.shape)
         # image = image.astype('uint8')
-        images.append(image)
+        #images.append(image)
+        images.append(cv2.imread(image, cv2.IMREAD_COLOR))
     return images
 
 # method takes a path and image and writes the image to that path
